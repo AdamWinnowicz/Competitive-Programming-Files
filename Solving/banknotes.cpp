27abeeba48bd2215a63ab48d29bc9e530c39c2ec
Adam Winnowicz <atomwinn@gmail.com>
2025-08-08 20:25:14 -0400

    Added correct file path for debug.h

---
+++ Solving/banknotes.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void test_case(){
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while(t--){
+        test_case();
+        //cout << (test_case() ? "Yes\n" : "No\n");
+    }
+
+    return 0;
+}   
